ca60149c02b0bef4fc883cc264cff597539ea258
per2 <per2@compsci-05.spusd.org>
2023-09-06 09:10:28 -0700

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    // public static final double PI = 3.14
+    // in kotlin, we use a companion object:
+    companion object {
+            // TAG is the default var ame for labeling your log statements
+            val TAG = "MainActivity"    
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: this is a log")
+    }
+
+    // to override an existing function, just start typing it
+    // can do this for the rest of the lifecycle functions (or any
+    // function that exists in the superclass)
+    override fun onStart() {
+                                        super.onStart()
+    }
+}
